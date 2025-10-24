b535738963a472db9899ad74feda4dcd122f43ea
Laure <agustin.laure@estudiante.imagecampus.edu.ar>
2025-10-24 13:04:20 -0300

    add bullets init function

--- src/Game/Bullet/Bullet.cpp
+++ src/Game/Bullet/Bullet.cpp
@@ -1,6 +1,38 @@
 #include "Game/Bullet/Bullet.h"
 
+#include "Game/Math/Vector/Vector.h"
+
 namespace bullet
 {
+	static void move(Bullet& bullet);
 
+
+	static void move(Bullet& bullet)
+	{
+		bullet.body.pos = vector::getVectorSum(bullet.body.pos, vector::getVectorMult(bullet.dir, bullet.speed));
+	}
+
+	void initBullets(Bullet bullets[])
+	{
+		for (int i = 0; i < maxBullets; i++)
+		{
+			bullets[i].body.pos = { 0,0 };
+			bullets[i].body.radius = bulletInitialRadius;
+			bullets[i].dmg = bulletInitialDmg;
+			bullets[i].speed = bulletInitialSpeed;
+			bullets[i].dir = { 0,0 };
+			bullets[i].isOn = false;
+		}
+	}
+
+	void update(Bullet bullets[])
+	{
+		for (int i = 0; i < maxBullets; i++)
+		{
+			if (bullets[i].isOn)
+			{
+				move(bullets[i]);
+			}
+		}
+	}
 }
--- src/Game/Bullet/Bullet.h
+++ src/Game/Bullet/Bullet.h
@@ -5,11 +5,20 @@
 namespace bullet
 {
 	const float bulletInitialDmg = 1.0f;
+	const float bulletInitialSpeed = 0.5f;
+	const float bulletInitialRadius = 5.0;
+	const int maxBullets = 100;
 
 	struct Bullet
 	{
 		form::Circle body;
 		float dmg = 0.0f;
-		
+		float speed = 0.0f;
+		Vector2 dir = { 1,0 };
+		bool isOn = false;
 	};
+
+	void initBullets(Bullet bullets[]);
+	void update(Bullet bullets[]);
+	
 }
